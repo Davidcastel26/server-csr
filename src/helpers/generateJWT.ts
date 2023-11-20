42f1a6838b8072e13cfbad52bfa108b5e092213e
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-20 17:46:07 -0600

    Update generateJWT.ts

--- src/helpers/generateJWT.ts
+++ src/helpers/generateJWT.ts
@@ -1,1 +1,33 @@
- 
+import { Secret, sign } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if( !process.env.SECRETORPRIVATEKEY ){
+    throw new Error('SECRETPRIVATEKEY environment variable is not defined');
+    // return;
+}
+
+
+export const generateJwt = ( uid: string ) => {
+    return new Promise( (resolve: Function, reject: Function) => {
+
+        const payload = { uid };
+
+        
+        const secretPrivateKey = process.env.SECRETORPRIVATEKEY as Secret || '';
+
+        // jwt.sign( payload, process.env.SECRETORPRIVATEKEY, {
+        sign( payload, secretPrivateKey , {
+            expiresIn:'4h'
+        }, (err: Error | any, token?: string) => {
+            
+            if( err ){
+                console.log(err);
+                reject('Cannot create token')
+            }else{
+                resolve(token)
+            }
+        })
+    })
+}
